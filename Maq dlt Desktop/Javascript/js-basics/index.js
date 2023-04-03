fc24d0ecf2bf2b4f379c5ccd5c064d63f2bc101e
joaosousagithub <96670711+joaosousagithub@users.noreply.github.com>
2023-04-03 11:48:11 +0100

    Add files via upload

---
+++ Maq dlt Desktop/Javascript/js-basics/index.js
@@ -0,0 +1,26 @@
+let namem = "Mosh";
+console.log(namem);
+
+const interestRate = 0.3;
+//interestRate = 1;
+
+console.log(interestRate);
+
+let person = {
+    namem: "Mosh",
+    age: 30
+};
+
+person.namem = "John";
+
+let selection = 'namem';
+person[selection] = "Filipe_";
+
+console.log(person);
+
+/////Arrays//////////////////////////
+let selectedColors = ['red', 'blue'];
+selectedColors[2] = "green";
+selectedColors[3] = 1;
+console.log(selectedColors);
+console.log("length: "+selectedColors.length);
